9ce2691209990964daec9b315389023d13f95850
peshka <peshka5@yandex.ru>
2018-12-08 23:26:35 +0300

    fix timestamp format

--- flower/static/js/clock.js
+++ flower/static/js/clock.js
@@ -1,7 +1,7 @@
 let clock_container = $("#clock");
 let server_time = clock_container.data("timestamp") * 1000;
 server_time = new Date(server_time);
-console.log("Server time", server_time);
+clock_container.html(server_time.toUTCString());
 
 setInterval(function(){
     server_time.setUTCSeconds(server_time.getUTCSeconds()+1);
